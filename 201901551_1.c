001661ac15ceceb99981303851426c2e8735ddbe
김멍몽 <wjdahrrla@gmail.com>
2023-11-27 16:28:55 +0900

    20231127_1

--- 201901551_1.c
+++ 201901551_1.c
@@ -3,41 +3,10 @@
 #include <time.h>
 
 const int size = 7; // 배열 크기
+int arr[size]; // 배열
 
-// 버블 정렬 함수
-void bubbleSort(int arr[]) {
-    int i, j;
-    int swapped; // 교환이 이루어졌는지 여부를 나타내는 변수
-
-    printf("=========== 버블 정렬 과정 ===========\n");
-
-    for (i = 0; i < size-1; i++) {
-        swapped = 0; // 초기에는 아직 교환이 이루어지지 않았음을 표시
-
-        for (j = 0; j < size-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                // 인접한 두 원소를 비교하여 필요한 경우 교환
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                swapped = 1; // 교환이 이루어졌음을 표시
-            }
-
-            // 현재 패스에서의 배열 상태를 출력
-            printf("%d ", arr[j]);
-        }
-        printf("\n");
-
-        // 만약 현재 패스에서 교환이 전혀 이루어지지 않았다면 이미 정렬된 상태이므로 루프 종료
-        if (swapped == 0)
-            break;
-    }
-    printf("======================================\n");
-}
-
-int main() {
-    int arr[size];
-
+// 랜덤 배열 생성 함수
+void randomArr(int arr[]) {
     // 난수 초기화
     srand(time(NULL));
 
@@ -48,16 +17,46 @@ int main() {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
 
-    // 버블 정렬 함수 호출
-    bubbleSort(arr);
+// 버블 정렬 함수
+void bubbleSort(int arr[]) {
+    int i, j;
+    int check; // 교환이 이루어졌는지 여부를 나타내는 변수
 
+    for (i = 0; i < size-1; i++) {
+        check = 0; // 초기에는 아직 교환이 이루어지지 않았음을 표시한다.
+
+        for (j = 0; j < size-i-1; j++) { // 이미 앞 패스에서 정렬된 부분은 정렬하지 않기 위해 size-i-1 범위까지만 반복한다.
+            if (arr[j] > arr[j+1]) { // 현재 원소가 다음 원소보다 크면 두 원소의 자리를 바꾼다.
+                // 교환을 수행한다.
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                check = 1; // 교환이 이루어졌음을 표시한다.
+            }
+        }
+
+        // check가 0이라면, 이미 정렬이 모두 이루워졌음을 뜻함으로 반복문을 종료한다.
+        if (check == 0)
+            break;
+    }
+}
+
+// 배열 출력 함수
+void printArr(int arr[]) {
     // 정렬된 배열 출력
     printf("정렬된 배열: ");
     for (int i=0; i < size; i++)
         printf("%d ", arr[i]);
 
     printf("\n");
+}
+
+int main() {
+    randomArr(arr); // 랜덤 배열 생성 함수 호출
+    bubbleSort(arr); // 버블 정렬 함수 호출
+    printArr(arr); // 배열 출력 함수 호출
 
     return 0;
 }
